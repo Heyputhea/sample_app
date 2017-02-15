1caab7d7eca9508bd8230469056bdc67a7613519
Heyputha <puthea800@gmail.com>
2017-02-15 15:50:53 +0700

    https://github.com/Heyputhea/sample_app1.git

--- db/migrate/20170214080830_create_users.rb
+++ db/migrate/20170214080830_create_users.rb
@@ -2,8 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :puthea800
-      t.puthea800@gmail.com :email
+      t.string :email
 
       t.timestamps
     end
